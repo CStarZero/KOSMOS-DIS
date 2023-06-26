15c6710f18c7eb16d9dd68fa412713c86df97728
Christopher Phillips <46461409+CStarZero@users.noreply.github.com>
2023-06-26 15:18:13 -0700

    Update classes.py

--- classes.py
+++ classes.py
@@ -201,11 +201,11 @@ class SciImage:
         plt.close()
 
 
-bias_instance = Bias()
-super_bias = bias_instance.make_superbias()
+# bias_instance = Bias()
+# super_bias = bias_instance.make_superbias()
 
-flat_instance = Flat()
-super_flat = flat_instance.make_superflat()
+# flat_instance = Flat()
+# super_flat = flat_instance.make_superflat()
 
-sci_img_instance = SciImage()
-sci_img = sci_img_instance.do_2d_reduction()
+# sci_img_instance = SciImage()
+# sci_img = sci_img_instance.do_2d_reduction()
